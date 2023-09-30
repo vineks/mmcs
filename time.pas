58312a3c2aa5100cb17fb36a2f6e10f5fac84787
Dan Terpilovskii <danterpil@gmail.com>
2023-09-30 11:24:27 +0300

    Новый файл

---
+++ time.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var year_v:=readinteger('введите текущий год:');
+  if (year_v mod 4=0) then
+     if (year_v mod 100 = 0) and (year_v mod 400 <>0) then
+       println('Год невисокосный')
+     else println('Год високосный')
+  else println('Год невисокосный');
+  
+end.
