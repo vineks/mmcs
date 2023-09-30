517b167557b36b836f6042831a0843d5747864f4
Dan Terpilovskii <danterpil@gmail.com>
2023-09-30 11:05:07 +0300

    Дополнил код

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  print('hello world');
+  var time:=readinteger('Какой текущий час?');
+  assert((time<24) and (time>-1));
+  case time of
+    4..10: print('Доброе утро, мир!');
+    11..16:print('Добрый день, мир!');
+    17..22: print('Добрый вечер, мир!');
+    else print ('Доброй ночи, мир!');
+  end;
 end.
