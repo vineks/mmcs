84dbe792a67ed401bf338d9fc9dce1f4caf20980
Dan Terpilovskii <danterpil@gmail.com>
2023-10-08 14:10:35 +0300

    Программа из домашнего тестирования

---
+++ hello2.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var language:string;
+  writeln('What language do you used in real life? (Write on english)');
+  readln(language);
+   case language of
+     'russian', 'Russian':println('Дорогой пользователь, приветствуем вас!');
+     'english', 'English': println('Dear user, we greet you!');
+     'polish', 'Polish': println('Drogi użytkowniku, witamy Cię!');
+     else println('Sorry, you are using a language that isn"t supported by the program');
+   end;
+end.
